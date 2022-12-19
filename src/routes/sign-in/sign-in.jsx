f00ab25a4fff8cc410b7c193ecaabb7304acd86c
yolowski96 <goranyolovski@gmail.com>
2022-12-20 00:56:37 +0200

    create sign up component, the form input component, button component

--- src/routes/sign-in/sign-in.jsx
+++ src/routes/sign-in/sign-in.jsx
@@ -1,18 +1,22 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../utils/firebase/firebase";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../utils/firebase/firebase";
+import SignUp from "../../components/sign-up/sign-up";
 
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
 
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    }
-
-    return (
-        <div>
-            <h1>Sign in page</h1>
-            <button onClick={logGoogleUser}>Sign in with Google</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Sign in page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <SignUp />
+    </div>
+  );
+};
 
 export default SignIn;
--- src/routes/utils/firebase/firebase.js
+++ src/routes/utils/firebase/firebase.js
@@ -1,14 +1,20 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA-wo4uOdO7Zd1D2V_cVB8JRVJuJnoMnXY",
-    authDomain: "capstone-c1466.firebaseapp.com",
-    projectId: "capstone-c1466",
-    storageBucket: "capstone-c1466.appspot.com",
-    messagingSenderId: "668183553889",
-    appId: "1:668183553889:web:fbbc39ae774590b6b50720"
+  apiKey: "AIzaSyA-wo4uOdO7Zd1D2V_cVB8JRVJuJnoMnXY",
+  authDomain: "capstone-c1466.firebaseapp.com",
+  projectId: "capstone-c1466",
+  storageBucket: "capstone-c1466.appspot.com",
+  messagingSenderId: "668183553889",
+  appId: "1:668183553889:web:fbbc39ae774590b6b50720",
 };
 
 // Initialize Firebase
@@ -17,30 +23,44 @@ const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-    prompt: "select_account"
-})
+  prompt: "select_account",
+});
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
-    const userDocRef = doc(db, 'users', userAuth.uid);
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
 
-    const userSnapshot = await getDoc(userDocRef);
+  const userDocRef = doc(db, "users", userAuth.uid);
 
-    if (!userSnapshot.exists()) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
+  const userSnapshot = await getDoc(userDocRef);
 
-        try {
-            await setDoc(userDocRef, { displayName, email, createdAt });
-        } catch (error){
-            console.log('error creating the user', error.meessage);
-        }
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(
+        userDocRef,
+        { displayName, email, createdAt, ...additionalInformation }
+      );
+    } catch (error) {
+      console.log("error creating the user", error.meessage);
     }
+  }
 
-    return userDocRef;
-}
+  return userDocRef;
+};
 
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
